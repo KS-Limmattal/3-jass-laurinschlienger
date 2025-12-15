02420d77dfd53af3676de6048b6f8e0cacf10e61
Laurin Schlienger <laurin.schlienger@stud.kslzh.ch>
2025-12-15 15:27:47 +0100

    Laurin

--- Deck.java
+++ Deck.java
@@ -28,11 +28,16 @@ import java.util.Arrays;
 public class Deck {
     Card[] cards;
 
+    //Konstruktor
+    public Deck(Card[] cards) {
+        this.cards = cards;
+    }
+
     // Konstruktor
     public Deck() {
         this.cards = new Card[36];
         int i = 0;
-        while (i > 36) {
+        while (i < 36) {
             for (Suit s : Suit.values()) {
                 for (Rank r : Rank.values()) {
                     Card Karte = new Card(r, s);
@@ -50,19 +55,19 @@ public class Deck {
 
     // addCard
     public void addCard(Card card) {
-        for (int i = 0; i > 36; i++) {
+        for (int i = 0; i < cards.length; i++) {
             if (card.equals(cards[i])) {
                 System.err.println("Diese Karte ist im Deck schon vorhanden.");
                 return;
             }
         }
         cards = Arrays.copyOf(cards, cards.length + 1);
-        cards[cards.length] = card;
+        cards[cards.length - 1] = card;
     }
 
     // PoP entfernt die Letste Karte eines Decks
     public Card pop() {
-        Card karte = cards[cards.length];
+        Card karte = cards[cards.length - 1];
         cards = Arrays.copyOf(cards, cards.length - 1);
         return karte;
     }
--- Jass.java
+++ Jass.java
@@ -6,16 +6,39 @@
  * Danach sollen alle Karten auf der Konsole ausgegeben werden.
  */
 public class Jass {
+    static Deck player1 = new Deck(new Card[0]);
+    static Deck player2 = new Deck(new Card[0]);
+    static Deck player3 = new Deck(new Card[0]);
+    static Deck player4 = new Deck(new Card[0]);
+    int punkte13 = 0;
+    int punkte24 = 0;
+public static void distributCards(){
+Deck deck = new Deck();
+deck.shuffle();
+for(int i = 0 ; i < 9; i++){
+    player1.addCard(deck.pop());
+}
+for(int i = 0 ; i < 9; i++){
+    player2.addCard(deck.pop());
+}
+for(int i = 0 ; i < 9; i++){
+    player3.addCard(deck.pop());
+}
+for(int i = 0 ; i < 9; i++){
+    player4.addCard(deck.pop());
+}
+}
     //Testcode
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        deck.shuffle();
-        for(int i= 0 ; i > 31; i++){
-            deck.pop();
-        }
-        Card eichelass = new Card(Rank.ASS, Suit.EICHELN);
-        deck.addCard(eichelass);
-        
+        // Deck deck = new Deck();
+        // deck.shuffle();
+        // for(int i= 0 ; i < 30; i++){
+        //     deck.pop();
+        // }
+        // Card eichelass = new Card(Rank.ASS, Suit.EICHELN);
+        // deck.addCard(eichelass);
+        // for (int i = 0; i < deck.cards.length;i++){
+        // System.out.println(deck.cards[i].toString());}
     }
     }
 
